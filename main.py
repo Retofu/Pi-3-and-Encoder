3264d6615b00aae377de5bb60d4cf4eaf7738b08
Retofu <leha.crivonosov@yandex.ru>
2025-09-11 12:19:48 +0500

    Переход на pigpio

--- main.py
+++ main.py
@@ -1,44 +1,56 @@
-import RPi.GPIO as GPIO
+import pigpio
 import math
 import time
 
-# Настройка пинов
-A_PIN = 17  # Фаза A (GPIO17, pin 11)
-B_PIN = 22  # Фаза B (GPIO22)
-Z_PIN = 27  # Фаза Z (GPIO27, pin 13)
+A_PIN = 17
+B_PIN = 22
+Z_PIN = 27
+PPR = 1200
 
-PPR = 1200  # Разрешение энкодера (импульсов на оборот). Уточните в даташите вашего энкодера!
+counter = 0
 
-counter = 0  # Счетчик импульсов
-angle_rad = 0.0  # Текущий угол в радианах
+pi = pigpio.pi()                   # подключение к демону
+if not pi.connected:
+    raise RuntimeError("pigpio daemon is not running")
 
-# Настройка GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(A_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(B_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(Z_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Входы с подтяжкой вверх
+pi.set_mode(A_PIN, pigpio.INPUT)
+pi.set_pull_up_down(A_PIN, pigpio.PUD_UP)
+pi.set_mode(B_PIN, pigpio.INPUT)
+pi.set_pull_up_down(B_PIN, pigpio.PUD_UP)
+pi.set_mode(Z_PIN, pigpio.INPUT)
+pi.set_pull_up_down(Z_PIN, pigpio.PUD_UP)
 
-# Обработчик прерывания для фазы A
-def handle_encoder_A(channel):
+# Небольшой фильтр дребезга (микросекунды)
+pi.set_glitch_filter(A_PIN, 200)
+pi.set_glitch_filter(B_PIN, 200)
+pi.set_glitch_filter(Z_PIN, 200)
+
+def handle_A(gpio, level, tick):
     global counter
-    if GPIO.input(A_PIN) == GPIO.input(B_PIN):
-        counter += 1  # Вращение по часовой стрелке
-    else:
-        counter -= 1  # Вращение против часовой стрелки
+    if level == pigpio.TIMEOUT:
+        return
+    # Квадратура: сравниваем текущие уровни A и B
+    a = pi.read(A_PIN)
+    b = pi.read(B_PIN)
+    counter += 1 if a == b else -1
 
-# Обработчик прерывания для фазы Z (индексный импульс)
-def handle_encoder_Z(channel):
+def handle_Z(gpio, level, tick):
     global counter
-    counter = 0  # Сброс счетчика на абсолютный ноль
+    if level == 1:  # RISING
+        counter = 0
 
-# Назначение прерываний
-GPIO.add_event_detect(A_PIN, GPIO.RISING, callback=handle_encoder_A, bouncetime=2)
-GPIO.add_event_detect(Z_PIN, GPIO.RISING, callback=handle_encoder_Z, bouncetime=2)
+cb_a = pi.callback(A_PIN, pigpio.EITHER_EDGE, handle_A)
+cb_z = pi.callback(Z_PIN, pigpio.RISING_EDGE, handle_Z)
+
 try:
     while True:
-        # Расчет угла в радианах
         angle_rad = (counter % PPR) * (2 * math.pi / PPR)
         print(f"Угол: {angle_rad:.3f} рад, Счетчик: {counter}")
-        time.sleep(0.01)  # Короткая задержка
+        time.sleep(0.01)
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    pass
+finally:
+    cb_a.cancel()
+    cb_z.cancel()
+    pi.stop()
